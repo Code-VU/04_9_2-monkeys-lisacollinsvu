fa3128d367264ec7b2d64cb85bbbb66497a01382
Lisa Collins <Lisa.Collins@veteransunited.com>
2025-02-07 08:54:10 -0600

    lisa monkey calculate

--- monkeyCalculator.py
+++ monkeyCalculator.py
@@ -4,6 +4,17 @@ def calculateTime():
     monkey_one = input("Is the first monkey smiling?:  ")
     monkey_two = input("Is the second monkey smiling?: ")
 
+    if monkey_one == "y":
+        if monkey_two == "y":
+            print("Uh Oh! We're in trouble!")
+        else:
+            print("Yay! We're going to have a good day!")
+
+    elif monkey_one == "n":
+        if monkey_two == "n":
+            print("Uh Oh! We're in trouble!")
+        else:
+            print("Yay! We're going to have a good day!")
     # end assignment
 
 
